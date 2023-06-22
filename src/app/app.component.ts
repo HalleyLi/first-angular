a67bec18463925ccf2631fad6dc8a48f5bcfb997
HaileyLi <1638343987@qq.com>
2023-06-21 23:57:31 -0400

    feat: add logger and httpClient fea

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AppService } from './app.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less']
+  styleUrls: ['./app.component.less'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(private appService: AppService)
+   {}
+
+  ngOnInit(): void {
+    this.appService.getHeroes();
+    this.appService.getBwics();
+  }
   title = 'first-angular';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { provideHttpClient } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { Logger } from './logger.service';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [Logger, provideHttpClient()],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
